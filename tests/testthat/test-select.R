f48909ec3bb2976144d8edbaf0a3e7ef1ff80796
Jonathan Keane <jkeane@gmail.com>
2019-10-08 18:50:52 -0500

    First tests

---
+++ tests/testthat/test-select.R
@@ -0,0 +1,20 @@
+context("Test fixture")
+
+con <- DBI::dbConnect(RSQLite::SQLite(), test_path("data/nycflights13.sqlite"))
+
+test_that("The fixture is what we expect", {
+  expect_identical(
+    DBI::dbListTables(con),
+    c("airlines", "flights", "sqlite_stat1", "sqlite_stat4")
+  )
+
+  expect_identical(
+    DBI::dbGetQuery(con, "SELECT * FROM airlines LIMIT 2"),
+    data.frame(
+      carrier = c("9E", "AA"),
+      name = c("Endeavor Air Inc.", "American Airlines Inc."),
+      stringsAsFactors = FALSE
+    )
+  )
+})
+
