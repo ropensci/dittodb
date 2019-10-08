5f1832006ab47cde953f7893e1b5b585452b91af
Pachamaltese <mvargas@dcc.uchile.cl>
2019-10-08 20:18:24 -0300

    in-memory nycflights13

--- nycflights13/create-db.R
+++ nycflights13/create-db.R
@@ -1,36 +1,47 @@
-library(nycflights13)
+#' Create an in-memory SQLite database for testing
+#'
+#' @return The function returns the summary of the estimated gravity model as an
+#' @export
+#'
+#' @examples
+#' nycflights13_sqlite()
+
+nycflights13_sqlite <- function() {
+  nycflights13_db <- src_sqlite(":memory:", create = TRUE)
+
+  copy_to(nycflights13_db, nycflights13::flights,
+          name = "flights",
+          indexes = list(
+            c("year", "month", "day"),
+            "carrier",
+            "tailnum"
+          ))
+
+  copy_to(nycflights13_db, nycflights13::airlines,
+          name = "airlines",
+          indexes = list("carrier")
+          )
+
+  return(nycflights13_db)
+}
+
+# Tests ---
+
 library(dplyr)
+library(nycflights13)
 
-db <- src_sqlite("nycflights13/nycflights13.sqlite", create = TRUE)
+d <- nycflights13_sqlite()
 
-# flights ----
-
-flights_sqlite <-
-  copy_to(
-    db, nycflights13::flights,
-    name = "flights",
-    temporary = FALSE,
-    indexes = list(
-      c("year", "month", "day"),
-      "carrier",
-      "tailnum"
-    ))
-
-flights_sqlite <- tbl(db, "flights")
-flights_sqlite
-
-# airlines ----
-
-airlines_sqlite <- copy_to(db, airlines)
+flights_sqlite <- tbl(d, "flights")
+airlines_sqlite <- tbl(d, "airlines")
 
 # top airlines in terms of number of flights
 top_airlines <- flights_sqlite %>%
   group_by(carrier) %>%
   summarize(n = n()) %>%
   arrange(desc(n))
-top_airlines
 
-# joins ----
+top_airlines
 
 top_airlines <- top_airlines %>% left_join(airlines_sqlite,  by = "carrier")
 top_airlines
