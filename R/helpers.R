d2e58e257aac9d2bfcf9df79764fd0ce3abd6e0e
Jonathan Keane <jkeane@gmail.com>
2019-10-11 10:01:58 -0500

    Remove helpers.R since that's now mostly in recording.R

--- R/helpers.R
+++
@@ -1,7 +0,0 @@
-# internal functions to easily turn on tracing for `dbGetQuery`/`dbFetchQuery`
-turn_on_query_tracing <- function() {
-  trace("dbGetQuery", quote(browser()),
-        signature = c(conn = "DBIConnection", statement = "character"))
-}
-
-
