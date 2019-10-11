ef1bc7f87d3ac121499913e089589e36433e09f4
Jonathan Keane <jkeane@gmail.com>
2019-10-10 20:03:21 -0500

    file path tests

---
+++ tests/testthat/test-paths.R
@@ -0,0 +1,13 @@
+context("File paths")
+
+test_that("path operations work", {
+  expect_identical(make_path("foo", "bar", "feef"), "foo/bar-feef.R")
+
+  expect_true(ensure_file(test_path("test-paths.R")))
+  expect_false(ensure_file(test_path("not-here.R")))
+
+  expect_error(
+    read_file("not-here.R"),
+    "Couldn't find the file at not-here.R"
+  )
+})
