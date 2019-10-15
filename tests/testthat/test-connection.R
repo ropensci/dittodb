6bcd1ad5c3708bfae7d466ca45be5218978b4f8f
Jonathan Keane <jkeane@gmail.com>
2019-10-15 18:24:49 -0500

    Unknown driver warning

--- tests/testthat/test-connection.R
+++ tests/testthat/test-connection.R
@@ -1,5 +1,13 @@
 context("Mock connection")
 
+test_that("Error handling", {
+  expect_warning(
+    dbMockConnect(data.frame(), dbname = "name"),
+    paste0(
+      "data.frame is an unknown driver, dbtest will have limited functionality."
+    ))
+})
+
 with_mock_db({
   test_that("Our connection is a mock connection", {
     # ":memory:" is non-portable, so using something close, but portable
