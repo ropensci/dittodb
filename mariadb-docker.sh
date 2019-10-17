9e0d91938f3506609fe42213173315a5777139f0
Jonathan Keane <jkeane@gmail.com>
2019-10-16 23:36:35 -0500

    Drop databases after testing

--- mariadb-docker.sh
+++ mariadb-docker.sh
@@ -7,12 +7,13 @@ docker pull mariadb
 docker run --name dbtest-mariadb -p 3306:3306 -e MYSQL_ROOT_PASSWORD=r2N5y7V* -d mariadb:latest
 
 # retry until mariadb is up (or 15 times)
+sleep 5
 n=0
 until [ $n -ge 15 ]
 do
   docker exec -it dbtest-mariadb mysql -pr2N5y7V* -e "CREATE DATABASE nycflights;" && break  # substitute your command here
   n=$[ $n+1 ]
-  sleep 15
+  sleep 5
 done
 
 docker exec -it dbtest-mariadb mysql -pr2N5y7V* -e "CREATE USER IF NOT EXISTS travis@'%'; GRANT ALL ON *.* TO travis@'%'; FLUSH PRIVILEGES;"
--- postgres-docker.sh
+++ postgres-docker.sh
@@ -7,12 +7,13 @@ docker pull postgres
 docker run --name dbtest-postgres -p 5432:5432 -e POSTGRES_USER=travis -d postgres:latest
 
 # retry until postgres is up (or 15 times)
+sleep 5
 n=0
 until [ $n -ge 15 ]
 do
-  docker exec -it -e PGPASSWORD=r2N5y7V -e POSTGRES_USER=travis dbtest-postgres psql -U travis -c "CREATE DATABASE nycflights;" && break  # substitute your command here
+  docker exec -it dbtest-postgres psql -U travis -c "CREATE DATABASE nycflights;" && break  # substitute your command here
   n=$[$n+1]
-  sleep 15
+  sleep 5
 done
 
 # docker exec -it -e PGPASSWORD=r2N5y7V -e POSTGRES_USER=travis dbtest-postgres psql -U travis -c "CREATE USER IF NOT EXISTS travis@'%' IDENTIFIED BY ''; GRANT ALL ON *.* TO travis@'%'; FLUSH PRIVILEGES;"
