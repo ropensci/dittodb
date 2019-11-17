f86736c73759dc6531a2510361e68081036e4fb0
Jonathan Keane <jkeane@gmail.com>
2019-11-17 09:26:58 -0600

    try this

--- db-setup/postgres-reset.sh
+++ db-setup/postgres-reset.sh
@@ -1,4 +1,4 @@
 # reset postgress databases in docker
-
+docker ps
 docker exec -it dbtest-postgres psql -U travis -c "DROP DATABASE IF EXISTS nycflights;"
 docker exec -it dbtest-postgres psql -U travis -c "CREATE DATABASE nycflights;"
