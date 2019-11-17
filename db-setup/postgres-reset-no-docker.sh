28bddce60d9455a2c7e7ee687c9b109725452053
Jonathan Keane <jkeane@gmail.com>
2019-11-17 00:57:19 -0600

    again

--- db-setup/postgres-reset-no-docker.sh
+++ db-setup/postgres-reset-no-docker.sh
@@ -1,3 +1,3 @@
 # reset postgress databases
-sudo -u postgres psql -U travis -c "DROP DATABASE IF EXISTS nycflights;"
-sudo -u postgres psql -U travis -c "CREATE DATABASE nycflights;"
+sudo -u postgres psql -c "DROP DATABASE IF EXISTS nycflights;"
+sudo -u postgres psql -c "CREATE DATABASE nycflights;"
