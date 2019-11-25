1918aa13fe5b12a74d06de0280d4924422b1fa12
Jonathan Keane <jkeane@gmail.com>
2019-11-25 09:22:27 -0600

    Some password shenanigans

--- db-setup/mariadb-brew.sh
+++ db-setup/mariadb-brew.sh
@@ -2,4 +2,6 @@ rm -f /etc/my.cnf
 brew install mariadb
 brew services start mariadb
 mkdir /usr/local/etc/my.cnf.d
+# TODO: add dbrootpass if it doesn't exist?
+sudo mysql -u root "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DBROOTPASS';"
 sleep 2
