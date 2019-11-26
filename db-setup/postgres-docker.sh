a6eef11144609f8fe081361ee2586de74fcd578c
Jonathan Keane <jkeane@gmail.com>
2019-11-26 10:19:59 -0600

    DB alignment

--- db-setup/postgres-docker.sh
+++ db-setup/postgres-docker.sh
@@ -4,14 +4,14 @@
 docker pull postgres
 
 # on macos, you can only run port forwards
-docker run --name dbtest-postgres -p 5432:5432 -e POSTGRES_USER=travis -d postgres:latest
+docker run --name dbtest-postgres -p 5432:5432 -e POSTGRES_USER=postgres -d postgres:latest
 
 # retry until postgres is up (or 15 times)
 n=0
 until [ $n -ge 15 ]
 do
   sleep 5
-  bash db-setup/postgres-reset-docker.sh && break  # substitute your command here
+  docker exec dbtest-postgres psql -U postgres < db-setup/postgres-reset.sql && break
   n=$[$n+1]
 done
 
