1ee4f1a4d2ff31c1203f2d68bce951a148ed9a44
Jonathan Keane <jkeane@gmail.com>
2019-12-05 20:01:37 -0600

    more localhost?

--- db-setup/mariadb-reset.sql
+++ db-setup/mariadb-reset.sql
@@ -1,10 +1,11 @@
 DROP DATABASE IF EXISTS nycflights;
 CREATE DATABASE nycflights;
 CREATE USER IF NOT EXISTS 'travis'@'%';
+CREATE USER IF NOT EXISTS 'travis'@'localhost';
 GRANT ALL ON *.* TO 'travis'@'%';
 GRANT ALL ON *.* TO 'travis'@'localhost';
 FLUSH PRIVILEGES;
 ALTER USER 'travis'@'%' IDENTIFIED BY '';
-ALTER USER 'travis'@'localhos' IDENTIFIED BY '';
+ALTER USER 'travis'@'localhost' IDENTIFIED BY '';
 FLUSH PRIVILEGES;
 
