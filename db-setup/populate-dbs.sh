23408f5f9ad4b035c4e9e50fb298a683329cf29e
Mauricio Vargas <mvargas@dcc.uchile.cl>
2020-04-24 18:15:10 -0400

    ports in populate-dbs.sh

--- db-setup/populate-dbs.sh
+++ db-setup/populate-dbs.sh
@@ -1,5 +1,5 @@
-psql -h 127.0.0.1 -U postgres -f db-setup/postgres-reset.sql
-psql -h 127.0.0.1 -U postgres -d nycflights -f db-setup/postgres-nycflights.sql
+psql -h 127.0.0.1 -U postgres -p 2345 -f db-setup/postgres-reset.sql
+psql -h 127.0.0.1 -U postgres -p 2345 -d nycflights -f db-setup/postgres-nycflights.sql
 
-mysql -h 127.0.0.1 -u root -e "source db-setup/mariadb-reset.sql"
-mysql -h 127.0.0.1 -u root -e "source db-setup/mariadb-nycflights.sql"
+mysql -h 127.0.0.1 -u root -p 6033 -e "source db-setup/mariadb-reset.sql"
+mysql -h 127.0.0.1 -u root -p 6033 -e "source db-setup/mariadb-nycflights.sql"
