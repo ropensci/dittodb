abd88cc631ff6229e724fa0d68c89a5250a35cac
Mauricio Vargas <mvargas@dcc.uchile.cl>
2020-04-25 18:04:16 -0400

    pfctl4

--- db-setup/postgres-brew.sh
+++ db-setup/postgres-brew.sh
@@ -15,6 +15,6 @@ initdb /usr/local/var/postgres
 pg_ctl -D /usr/local/var/postgres start
 /usr/local/opt/postgres/bin/createuser -s postgres
 echo "
-rdr pass proto tcp from any to any port 2345 -> 127.0.0.1 port 5432
+rdr pass inet proto tcp from any to any port 2345 -> 127.0.0.1 port 5432
 " | pfctl -ef -
 sleep 2
