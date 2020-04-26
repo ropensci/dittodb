a9c74759f6fe1717f254491af7fb2c620fc2fadd
Jonathan Keane <jkeane@gmail.com>
2020-04-25 22:32:24 -0500

    Add functionality to start and stop dittodb mock usage

---
+++ tests/testthat/test-start-stop-mocks.R
@@ -0,0 +1,25 @@
+start_mock_db()
+
+con <- dbConnect(RSQLite::SQLite(), dbname = "new_db")
+test_that("The connection has a new path", {
+  expect_identical(con@path, "new_db")
+})
+
+test_that("We can use mocks from the new path", {
+  expect_identical(
+    dbGetQuery(con, "SELECT * FROM airlines LIMIT 3"),
+    data.frame(
+      carrier = c("9E", "AA", "AS"),
+      name = c(
+        "Endeavor Air Inc.",
+        "American Airlines Inc.",
+        "Alaska Airlines Inc."
+      ),
+      stringsAsFactors = FALSE
+    )
+  )
+})
+
+dbDisconnect(con)
+
+stop_mock_db()
