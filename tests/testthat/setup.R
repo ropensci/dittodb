9e4a506072f0cbf30a7c04abf3461aebedf81526
Jonathan Keane <jkeane@gmail.com>
2020-04-25 23:39:44 -0500

    windows 64 versus 32

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -11,13 +11,8 @@ db_user <- "travis"
 db_pass <- "*V7y5N2r"
 
 
-windows <- any(
-  .Platform$OS.type == "windows",
-  tolower(Sys.info()['sysname']) == "windows",
-  na.rm = TRUE
-)
-
-odbc_driver <- if (windows) {
+win_x64 <- .Platform$OS.type == "windows" & Sys.info()[["machine"]] == "x86_64"
+odbc_driver <- if (win_x64) {
   "PostgreSQL Unicode(x64)"
 } else {
   "PostgreSQL Unicode"
