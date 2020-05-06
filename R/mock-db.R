464d5d78a49a2ee4087290c7f3e27a95ddacfafd
Jonathan Keane <jkeane@gmail.com>
2020-05-05 19:30:49 -0500

    More examples, now more runnable (#86)

--- R/mock-db.R
+++ R/mock-db.R
@@ -1,15 +1,19 @@
 #' Run DBI queries against a mocked database
 #'
-#' Start and stop using a mocked database with `start_mock_db()` and
-#' `stop_mock_db()` respectively. Alternatively, you can wrap a chunk of code in
-#' `with_mock_db()` to execute the whole thing without needing to remember to
-#' stop the mocking.When testing with `dittodb`, it will look for fixtures in
-#'  all entries of [`db_mock_paths`].
+#' Wrap a chunk of code in `with_mock_db()` to use mocked databases that will
+#' use fixtures instead of connecting to a real database. Alternatively, you can
+#' start and stop using a mocked database with `start_mock_db()` and
+#' `stop_mock_db()` respectively.to execute the whole thing without needing to
+#' remember to stop the mocking. When testing with `dittodb`, it will look for
+#' fixtures in all entries of [`db_mock_paths`].
 #'
 #' You only need to use one approach: either use `start_mock_db()` to start
 #' using mocks and then `stop_mock_db()` to stop or use `with_mock_db()` wrapped
 #' around the code you want to execute against the mocked database. You don't
-#' need to (and should not) use both at the same time.
+#' need to (and should not) use both at the same time. Generally
+#' `with_mock_db()` is preferred because it is slightly safer and you don't have
+#' to remember to `stop_mock_db()` when you're done. However, it is easier to
+#' step through tests interactively using `start_mock_db()`/`stop_mock_db()`.
 #'
 #' Connections should be made after `start_mock_db()` if you're using that
 #' function or they should be made inside of `with_mock_db()` if you're using
@@ -27,13 +31,35 @@
 #'
 #' @examples
 #' \dontrun{
-#' start_mock_db()
+#' # Add the mocks included with dittodb to the db_mock_paths to use them below
+#' db_mock_paths(system.file("nycflight_mocks", package = "dittodb"), last = TRUE)
 #'
+#' # using  `with_mock_db()`
+#' with_mock_db({
+#'   con <- dbConnect(
+#'     RSQLite::SQLite(),
+#'     dbname = "nycflights"
+#'   )
+#'
+#'   test_that("We get one airline", {
+#'     one_airline <- dbGetQuery(
+#'       con,
+#'       "SELECT carrier, name FROM airlines LIMIT 1"
+#'     )
+#'     expect_is(one_airline, "data.frame")
+#'     expect_equal(nrow(one_airline), 1)
+#'     expect_equal(one_airline$carrier, "9E")
+#'     expect_equal(one_airline$name, "Endeavor Air Inc.")
+#'   })
+#' })
+#'
+#' # using `start_mock_db()` and `stop_mock_db()` (and a different driver)
+#' start_mock_db()
 #' con <- dbConnect(
 #'   RPostgreSQL::PostgreSQL(),
 #'   dbname = "nycflights",
 #'   host = "127.0.0.1",
-#'   user = "travis",
+#'   user = "some_user",
 #'   password = ""
 #' )
 #'
@@ -48,29 +74,8 @@
 #'   expect_equal(one_airline$name, "Endeavor Air Inc.")
 #' })
 #'
+#' dbDisconnect(con)
 #' stop_mock_db()
-#'
-#' # or use `with_mock_db()`
-#' with_mock_db({
-#'   con <- dbConnect(
-#'     RPostgreSQL::PostgreSQL(),
-#'     dbname = "nycflights",
-#'     host = "127.0.0.1",
-#'     user = "travis",
-#'     password = ""
-#'   )
-#'
-#'   test_that("We get one airline", {
-#'     one_airline <- dbGetQuery(
-#'       con,
-#'       "SELECT carrier, name FROM airlines LIMIT 1"
-#'     )
-#'     expect_is(one_airline, "data.frame")
-#'     expect_equal(nrow(one_airline), 1)
-#'     expect_equal(one_airline$carrier, "9E")
-#'     expect_equal(one_airline$name, "Endeavor Air Inc.")
-#'   })
-#' })
 #' }
 with_mock_db <- function(expr) {
   # TODO: change this to use start_mock_db()?
--- R/mock-paths.R
+++ R/mock-paths.R
@@ -10,6 +10,11 @@
 #' `db_mock_paths`, the result effectively moves that path to the first
 #' position.
 #'
+#' When you are capturing fixtures (e.g. with [`start_db_capturing`]), the first
+#' path is used as the path to save the fixtures in. For this reason, you may
+#' want to set the `last` argument to `TRUE` if you want to read from a
+#' directory but don't want to write to it.
+#'
 #' For finer-grained control, or to completely override the default behavior
 #' of searching in the current working directory, you can set the option
 #' "dittodb.mock.paths" directly.
@@ -20,20 +25,30 @@
 #' The function `.db_mock_paths` is the same as `db_mock_paths` although it is
 #' deprecated and should not be used.
 #'
-#' @param new Either a character vector of path(s) to add, or `NULL` to reset
+#' @param new either a character vector of path(s) to add, or `NULL` to reset
 #' to the default.
+#' @param last a logical, should the new path given be added to the end of the
+#' list of paths? (default: `FALSE`)
+#'
 #' @return If `new` is omitted, the function returns the current search paths, a
 #' character vector. If `new` is provided, the updated value will be returned
 #' invisibly.
 #' @examples
+#' \dontrun{
+#' # reset mock paths to default
+#' db_mock_paths(NULL)
+#'
 #' identical(db_mock_paths(), c("tests/testthat/", "."))
 #' db_mock_paths("/var/somewhere/else")
 #' identical(db_mock_paths(), c("/var/somewhere/else", "tests/testthat/", "."))
 #' db_mock_paths(NULL)
 #' identical(db_mock_paths(), c("tests/testthat/", "."))
+#' db_mock_paths("/var/somewhere/else", last = TRUE)
+#' identical(db_mock_paths(), c("tests/testthat/", ".", "/var/somewhere/else"))
+#' }
 #' @name mockPaths
 #' @export
-db_mock_paths <- function(new) {
+db_mock_paths <- function(new, last = FALSE) {
   # use both "." and testthat::test_path(".") in case they are different
   def <- unique(c("tests/testthat/", "."))
 
@@ -44,13 +59,19 @@ db_mock_paths <- function(new) {
   } else if (is.null(new)) {
     ## We're calling the function to reset to the default
     options(dittodb.mock.paths = new)
-    return(invisible(current))
   } else {
     ## We're adding one or more paths
-    current <- unique(c(new, current))
+    if (last) {
+      # if we are post-pending
+      current <- unique(c(current, new), fromLast = TRUE)
+    } else {
+      # if we are pre-pending
+      current <- unique(c(new, current))
+    }
     options(dittodb.mock.paths = current)
-    return(invisible(current))
   }
+
+  return(invisible(current))
 }
 
 # for backwards compatibility
