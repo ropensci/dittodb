151b9871a7abed6ef00eb93d9a5036f0c8291286
Jonathan Keane <jkeane@gmail.com>
2020-05-31 18:00:47 -0500

    Clean up linting

--- R/use-dittodb.R
+++ R/use-dittodb.R
@@ -11,9 +11,9 @@
 #' @importFrom utils tail
 #' @return Nothing: called for file system side effects.
 #' @export
-use_dittodb <- function (path=".") {
+use_dittodb <- function(path = ".") {
   if (!("DESCRIPTION" %in% dir(path))) {
-    stop(path, " is not an R package directory", call.=FALSE)
+    stop(path, " is not an R package directory", call. = FALSE)
   }
   add_dittodb_to_desc(file.path(path, "DESCRIPTION"))
   # TODO: could allow helper.r too
@@ -21,48 +21,48 @@ use_dittodb <- function (path=".") {
   invisible()
 }
 
-add_dittodb_to_desc <- function (file) {
+add_dittodb_to_desc <- function(file) {
   # Read DESCRIPTION, add dittodb to Suggests if not already there
 
   # Hack to preserve whitespace: read it twice
   desc_fields <- colnames(read.dcf(file))
-  desc <- read.dcf(file, keep.white=desc_fields)
+  desc <- read.dcf(file, keep.white = desc_fields)
   if (!("Suggests" %in% desc_fields)) {
     # Add a column for Suggests
-    desc <- cbind(desc, matrix("", ncol=1, dimnames=list(NULL, "Suggests")))
+    desc <- cbind(desc, matrix("", ncol = 1, dimnames = list(NULL, "Suggests")))
   }
-  if (!grepl("dittodb", desc[,"Suggests"])) {
+  if (!grepl("dittodb", desc[, "Suggests"])) {
     # Add dittodb
 
     # Parse the list, and try to preserve the whitespace from the original
-    suggested_pkgs <- unlist(strsplit(desc[,"Suggests"], ","))
+    suggested_pkgs <- unlist(strsplit(desc[, "Suggests"], ","))
     sep <- sub("^([[:blank:]\n]*).*", "\\1", suggested_pkgs)
     suggested_pkgs <- sort(c(trimws(suggested_pkgs), "dittodb"))
     extra_sep <- tail(unique(sep), 1)
-    if (length(extra_sep) == 0 || nchar(extra_sep) == 0) {
+    if (length(extra_sep)  == 0 || nchar(extra_sep) == 0) {
       extra_sep <- " "
     }
     sep <- c(sep, extra_sep)
-    desc[,"Suggests"] <- paste0(sep, suggested_pkgs, collapse=",")
+    desc[, "Suggests"] <- paste0(sep, suggested_pkgs, collapse = ",")
 
     # Msg and write
     message("Adding 'dittodb' to Suggests in DESCRIPTION")
-    write.dcf(desc, file=file, keep.white=desc_fields)
+    write.dcf(desc, file = file, keep.white = desc_fields)
   }
 }
 
-add_dittodb_to_helper <- function (file) {
+add_dittodb_to_helper <- function(file) {
   # Create tests/testthat/helper.R if it does not exist
 
   if (!file.exists(file)) {
     message("Creating ", file)
     message("Adding library(dittodb) to ", file)
-    dir.create(dirname(file), showWarnings=FALSE, recursive=TRUE)
-    cat("library(dittodb)\n", file=file)
+    dir.create(dirname(file), showWarnings = FALSE, recursive = TRUE)
+    cat("library(dittodb)\n", file = file)
     # Msg and write
   } else {
     helper_lines <- readLines(file)
-    if (!any(grepl("library(dittodb)", helper_lines, fixed=TRUE))) {
+    if (!any(grepl("library(dittodb)", helper_lines, fixed = TRUE))) {
       # Add "library(dittodb)" to the top if it's not already there
       helper_lines <- c("library(dittodb)", helper_lines)
       # Msg and write
--- tests/testthat/test-use-dittodb.R
+++ tests/testthat/test-use-dittodb.R
@@ -1,8 +1,8 @@
-test_add_to_desc <- function (str, msg="Adding 'dittodb' to Suggests") {
+test_add_to_desc <- function(str, msg = "Adding 'dittodb' to Suggests") {
   f <- tempfile()
-  cat(str, file=f)
+  cat(str, file = f)
   expect_message(add_dittodb_to_desc(f), msg)
-  setNames(read.dcf(f, keep.white="Suggests")[,"Suggests"], NULL)
+  setNames(read.dcf(f, keep.white = "Suggests")[, "Suggests"], NULL)
 }
 
 test_that("add to desc adds Suggests if not present", {
@@ -43,16 +43,16 @@ test_that("add to desc adds Suggests multiline", {
 })
 
 test_that("add to desc doesn't add if already present", {
-  expect_identical(test_add_to_desc("Suggests: dittodb", msg=NA), "dittodb")
-  expect_identical(test_add_to_desc("Suggests: pkg, dittodb", msg=NA),
+  expect_identical(test_add_to_desc("Suggests: dittodb", msg = NA), "dittodb")
+  expect_identical(test_add_to_desc("Suggests: pkg, dittodb", msg = NA),
                    "pkg, dittodb")
 })
 
-expect_added_to_helper <- function (str, msg="Adding library\\(dittodb\\)") {
+expect_added_to_helper <- function(str, msg = "Adding library\\(dittodb\\)") {
   f <- tempfile()
-  cat(str, file=f)
+  cat(str, file = f)
   expect_message(add_dittodb_to_helper(f), msg)
-  expect_true(any(grepl("library(dittodb)", readLines(f), fixed=TRUE)))
+  expect_true(any(grepl("library(dittodb)", readLines(f), fixed = TRUE)))
 }
 
 test_that("add to helper creates file if doesn't exist", {
@@ -68,7 +68,7 @@ test_that("add to helper adds", {
 })
 
 test_that("add to helper doesn't duplicate", {
-  expect_added_to_helper("library(dittodb)\n", msg=NA)
+  expect_added_to_helper("library(dittodb)\n", msg = NA)
 })
 
 test_that("use_dittodb integration test", {
@@ -77,7 +77,7 @@ test_that("use_dittodb integration test", {
   expect_error(use_dittodb(testpkg), "is not an R package directory")
 
   desc <- file.path(testpkg, "DESCRIPTION")
-  cat("Title: Foo\n", file=desc)
+  cat("Title: Foo\n", file = desc)
   helper <- file.path(testpkg, "tests", "testthat", "helper.R")
   expect_message(use_dittodb(testpkg))
   expect_identical(readLines(desc), c("Title: Foo", "Suggests: dittodb"))
