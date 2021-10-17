6f7f9f078fff8a18fad01d1260ce0ad2c8092603
Jonathan Keane <jkeane@gmail.com>
2021-10-17 16:53:26 -0500

    Add quoting tests

---
+++ tests/testthat/test-quote.R
@@ -0,0 +1,20 @@
+test_that("quoting works", {
+  con_mockmaria <- con_rpostgres <- list()
+  class(con_mockmaria) <- "DBIMockMariaDBConnection"
+  class(con_rpostgres) <- "DBIMockRPostgresConnection"
+
+  some_sql <- SQL("foo")
+
+  expect_identical(dbQuoteString(con_mockmaria, some_sql), some_sql)
+  expect_identical(dbQuoteString(con_rpostgres, some_sql), some_sql)
+
+  expect_identical(dbQuoteIdentifier(con_mockmaria, some_sql), some_sql)
+  expect_identical(dbQuoteIdentifier(con_rpostgres, some_sql), some_sql)
+
+  expect_identical(dbQuoteString(con_mockmaria, "foo"), SQL("'foo'"))
+  expect_identical(dbQuoteString(con_rpostgres, "foo"), SQL("'foo'"))
+  expect_identical(dbQuoteIdentifier(con_rpostgres, "foo"), SQL('"foo"'))
+  # However, MariaDB is slightly different (we haven't redefined it, since it
+  # doesn't communicate with the DB first)
+  expect_identical(dbQuoteIdentifier(con_mockmaria, "foo"), SQL('`foo`'))
+})
