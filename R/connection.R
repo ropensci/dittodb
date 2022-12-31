58cc7a30238bd073c2027d8ce2e5d776c8488d16
Jonathan Keane <jkeane@gmail.com>
2022-12-31 09:04:26 -0600

    Appease the lintr

--- R/connection.R
+++ R/connection.R
@@ -81,7 +81,8 @@ dbMockConnect <- function(drv, ...) {
 
   path <- get_dbname(dots)
 
-  # retrieve the dbplyr_edition for the original connection, if it exists (and if dbplyr is installed)
+  # retrieve the dbplyr_edition for the original connection, if it exists (and
+  # if dbplyr is installed)
   if (requireNamespace("dbplyr", quietly = TRUE)) {
     orig_dbplyr_edition <- tryCatch(
       getFromNamespace(paste0("dbplyr_edition.", original_class), "dbplyr"),
@@ -91,7 +92,7 @@ dbMockConnect <- function(drv, ...) {
     )
 
     # register dbplyr_edition
-    s3_register("dbplyr::dbplyr_edition", mock_class, method = orig_dbplyr_edition )
+    s3_register("dbplyr::dbplyr_edition", mock_class, method = orig_dbplyr_edition)
   }
 
   return(new(mock_class, path = path, original_class = original_class))
