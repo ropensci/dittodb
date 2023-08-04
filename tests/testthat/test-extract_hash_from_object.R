32dc23c2c6e84e950a96b70ee24dab573c370730
Bernhardt Straub <bernhardt.straub.extern@bitmarck.de>
2023-08-04 11:57:36 +0200

    test: add unit tests for extract_hash_from_object

---
+++ tests/testthat/test-extract_hash_from_object.R
@@ -0,0 +1,57 @@
+test_that("extract_hash_from_object string objects work", {
+  # test strings
+  expect_identical(extract_hash_from_object("foo"), "bd40ef")
+  expect_identical(extract_hash_from_object(""), "2f88e1")
+
+})
+
+test_that("extract_hash_from_object s4 MariaDBResult work", {
+  # create S4-Object for testing
+  obj <- new("MariaDBResult")
+  obj@sql <- "Select * from my_table"
+  expect_identical(extract_hash_from_object(obj), "e00bce")
+
+})
+
+test_that("extract_hash_from_object s4 PqResult work", {
+  # create S4-Object for testing
+  obj <- new("PqResult")
+  obj@sql <- "Select * from my_table"
+  expect_identical(extract_hash_from_object(obj), "e00bce")
+
+})
+
+test_that("extract_hash_from_object s4 SQLiteResult work", {
+  # create S4-Object for testing
+  obj <- new("SQLiteResult")
+  obj@sql <- "Select * from my_table"
+  expect_identical(extract_hash_from_object(obj), "e00bce")
+
+})
+
+test_that("extract_hash_from_object s4 OdbcResult work", {
+  # create S4-Object for testing
+  obj <- new("OdbcResult")
+  obj@statement <- "Select * from my_table"
+  expect_identical(extract_hash_from_object(obj), "e00bce")
+
+})
+
+test_that("extract_hash_from_object s4 DBIMockUnknownDBResult work", {
+  setClass("DBIMockUnknownDBResult",
+           slots = c(
+             m_sOperation = "character"
+           ),
+           prototype = list(
+             m_sOperation = NA_character_
+           )
+  )
+  setMethod("toString", "DBIMockUnknownDBResult", function(x) {
+    return("DBIMockUnknownDBResult")
+  })
+
+  # create S4-Object for testing
+  obj <- new("DBIMockUnknownDBResult")
+  obj@m_sOperation <- "Select * from my_table"
+  expect_identical(extract_hash_from_object(obj), "10a362")
+})
